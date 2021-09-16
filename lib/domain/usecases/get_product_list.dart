8107022e13e2baab4a36572d59d5ffe6024a59cc
Widyarso Joko Purnomo <widyjp10@gmail.com>
2021-09-16 23:19:05 +0700

    add get product list usecase implementation

--- lib/domain/usecases/get_product_list.dart
+++ lib/domain/usecases/get_product_list.dart
@@ -1,1 +1,12 @@
-class GetProductList {}
+import 'package:kulina_app/domain/entities/product.dart';
+import 'package:kulina_app/domain/repositories/product_repository.dart';
+
+class GetProductList {
+  final ProductRepository _repository;
+
+  GetProductList(this._repository);
+
+  Future<List<Product>> execute() {
+    return _repository.getProductList();
+  }
+}
